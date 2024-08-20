a4ac157470faadfeb17b3168c8055ab3ea9ae98d
Emmanuel Reynoso <32149800+Emmanuel-Reynoso@users.noreply.github.com>
2024-08-20 00:29:40 +0000

    Funcionamiento de routers

--- app/src/main.py
+++ app/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from items.items import router as items_router  # Importa el enrutador
+import uvicorn  # Importa Uvicorn
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ app.include_router(items_router)
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+# Bloque para ejecutar Uvicorn directamente
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
