4b1adf7b6c1e2ed7971badd9455e68b41998deaa
Emmanuel Reynoso <32149800+Emmanuel-Reynoso@users.noreply.github.com>
2024-08-19 21:34:17 -0300

    .

--- app/src/main.py
+++ app/src/main.py
@@ -10,8 +10,12 @@ app.include_router(items_router)
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+<<<<<<< HEAD
+=======
 
 # Bloque para ejecutar Uvicorn directamente
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
+
+>>>>>>> a4ac157470faadfeb17b3168c8055ab3ea9ae98d
